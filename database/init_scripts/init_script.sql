6e3e57d8a452d1d798420b7a30c8c8968e7e9cb2
lyudommuny13 <lyudommuny.kh.13@gmail.com>
2026-09-15 09:12:27 +0700

    first commit

---
+++ database/init_scripts/init_script.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE db_order OWNER ecommerce_usr;
+CREATE DATABASE db_payment OWNER ecommerce_usr;
+CREATE DATABASE db_business OWNER ecommerce_usr;
+CREATE DATABASE db_customer OWNER ecommerce_usr;
